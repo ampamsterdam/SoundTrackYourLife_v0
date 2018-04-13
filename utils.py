8388d648350bf4afff5c81b4409277f59ea199d7
jacopo solari <jaco.solari@gmail.com>
2018-04-13 11:56:02 +0200

    added regex function in utils.py to automatically get urls from feed txt file

--- utils.py
+++ utils.py
@@ -2,5 +2,11 @@ import os
 import re
 
 def get_mp3_url():
-    feed=with open('feed.txt') as input
+
+
+    with open('feed.txt') as myinput:
+        contents=myinput.read()
+    mymatch=re.findall(r'http(.+?)mp3',contents)
+    allmymatches=['https' + mymatch[x] + 'mp3' for x in range(len(mymatch))]
+print(allmymatches)
 
