efccbbbdc1578a1fcc514bda48d0c137c072321a
jacopo solari <jaco.solari@gmail.com>
2018-04-20 12:28:49 +0200

    implemented function in utils.py that retrieves the files from the ftp server and saves them in a txt

--- utils.py
+++ utils.py
@@ -2,29 +2,38 @@ import os
 import re
 import numpy as np
 from ftplib import FTP_TLS
+import ftplib
 
 def get_mp3_urls():
 
-    ftp=FTP_TLS()
-    ftp.set_debuglevel(2)
-    ftp.connect('91.184.0.39', 21)
-    ftp.sendcmd('USER f307123')
-    ftp.sendcmd('PASS 8AsREl8I6T7X')
+#connect to the ftp server and lists the tracks names in the database
+    ftp = ftplib.FTP("91.184.0.39")
+    ftp.login("f307123", "8AsREl8I6T7X")
+
     urls=ftp.nlst('webspace/httpdocs/eysoundtrack.com/resources/audio')
+    ftp.quit()
 
-    ftp.close()
+#alternatively
+    # ftp=FTP_TLS()
+    # ftp.set_debuglevel(2)
+    # ftp.connect('91.184.0.39', 21)
+    # ftp.sendcmd('USER f307123')
+    # ftp.sendcmd('PASS 8AsREl8I6T7X')
+    # urls=ftp.nlst('webspace/httpdocs/eysoundtrack.com/resources/audio')
+    # ftp.close()
 
-    myfile=open('urls.txt','wr')
-    myfile.writelines(["%s \n"%allmymatches[x] for x in range(len(allmymatches))])
-    myfile.close()
+#extracts and build the urls to use in the audio player directives
+#save them in the url.txt file
 
     # with open('feed.txt') as myinput:
     #     contents=myinput.read()
     # mymatch=re.findall(r'http(.+?)mp3',contents)
     # allmymatches=['https' + mymatch[x] + 'mp3' for x in range(len(mymatch))]
     #
-    # myfile=open('urls.txt','wr')
-    # myfile.writelines(["%s \n"%allmymatches[x] for x in range(len(allmymatches))])
-    # myfile.close()
+
+    basepath='https://eysoundtrack.com/resources/audio/'
+    myfile=open('urls.txt','wr')
+    myfile.writelines(["%s \n"%basepath + urls[x].split('/')[-1]  for x in range(len(urls))])
+    myfile.close()
 
     return 0
